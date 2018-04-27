3064e574e4b506289283bf1713d3cc052fbf87a7
tonlion <1633537611@qq.com>
2018-04-27 18:48:52 +0800

    修改请求

--- voice/src/main/java/com/zhang/video/voice/MainActivity.kt
+++ voice/src/main/java/com/zhang/video/voice/MainActivity.kt
@@ -1,48 +1,53 @@
 package com.zhang.video.voice
 
 import android.app.Activity
-import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import com.zhang.video.voice.`interface`.RequestService
-import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.OkHttpClient
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
+    var handler: Handler? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 //        fm_List.adapter = FMAdapter(this,null)
-
-val thread = thread {
-    Runnable {
         var builder = Retrofit.Builder().baseUrl("http://api.kufm.cn/").addConverterFactory(GsonConverterFactory.create());
         var retrofit = builder.client(OkHttpClient()).build()
         var client = retrofit.create(RequestService::class.java)
-        val call = client.getChannel(1,1)
-        var c = call.execute().body()
-        print(c.toString())
-    }
-}
-        thread.start()
-//        Thread { Runnable {
-//            var builder = Retrofit.Builder().baseUrl("http://api.kufm.cn/").addConverterFactory(GsonConverterFactory.create());
-//            var retrofit = builder.client(OkHttpClient()).build()
-//            var client = retrofit.create(RequestService::class.java)
-//            val call = client.getChannel(1,1)
-//            var c = call.execute().body()
-//            print(c.toString())
-//        } }.start()
+        val call = client.getChannel()
+        call.enqueue(object : Callback<ResponseBody> {
+            override fun onFailure(call: Call<ResponseBody>?, t: Throwable?) {}
+
+            override fun onResponse(call: Call<ResponseBody>?, response: Response<ResponseBody>?) {
+                var body = response?.body()
+                Log.e("這還是問題", body?.string())
+            }
+
+        })
+
+        handler = object : Handler() {
+            override fun handleMessage(msg: Message?) {
+                super.handleMessage(msg)
+                Log.e("异常", msg?.obj.toString());
+            }
+        }
     }
 
-    class FMAdapter(activity:Activity,data:Map<String,String>?) : BaseAdapter() {
+    class FMAdapter(activity: Activity, data: Map<String, String>?) : BaseAdapter() {
 
         override fun getItem(p0: Int): Any {
             TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
--- voice/src/main/java/com/zhang/video/voice/interface/RetrofitInterface.kt
+++ voice/src/main/java/com/zhang/video/voice/interface/RetrofitInterface.kt
@@ -6,7 +6,8 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface RequestService{
-    @GET("program_info_list/type/{type}/type_key/{key}")
-    fun getChannel(@Query("type") type:Int,@Query("key") key:Int):Call<ResponseBody>
+    @GET("program_info_list/type/1/type_key/1")
+//    fun getChannel(@Query("type1") type:Int,@Query("key1") key:Int):Call<ResponseBody>
+    fun getChannel():Call<ResponseBody>
 }
 
